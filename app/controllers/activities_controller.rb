2b6e9d233398e906b7a892f9a95f12813438bf35
Eduardo Marques dos Reis <54919290+EduardoMdR@users.noreply.github.com>
2020-12-10 20:48:20 -0300

    Testing if it is monitor and admin

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -1,2 +1,6 @@
 class ActivitiesController < ApplicationController
+  before_action :require_tutor, only: [:index]
+  def index
+    @act = Activity.all
+  end
 end
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,4 +6,16 @@ class ApplicationController < ActionController::Base
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: %i[name])
   end
+
+  def require_tutor
+    if current_user.present?
+      if current_user.role == "aluno"
+        flash[:error] = "Você precisa ser um monitor para entrar nessa página!"
+        redirect_to "/"
+      end
+    else
+      flash[:error] = "Você precisa estar logado para entrar nessa página!"
+      redirect_to "/"
+    end
+  end
 end
