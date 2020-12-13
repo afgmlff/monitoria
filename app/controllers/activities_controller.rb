2320db800b21ac2813826f460fa31f2050ac715c
afgmlff <aeciofgm@gmail.com>
2020-12-13 17:33:09 -0300

    test 13: activity controller get index and new successful

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -9,4 +9,24 @@ class ActivitiesController < ApplicationController
     # Lista todas as monitorias
     @activities = Activity.all
   end
+
+  ##
+  # GET /activity/:id
+  # Demonstração em detalhes os dados de um registro anteriormente criado
+  # para visualização
+  def show
+    # De acordo com o id passado pelo usuário (quando clica na monitoria) as informações são atualizadas
+    @activity = Activity.find(params[:id])
+  end
+
+  ##
+  # Ação new da classe Activity.
+  # Renderiza página para criação de uma monitoria.
+  # GET /activities/new
+  def new
+    # Renderiza uma página para inserção de dados
+    @activity = Activity.new
+  end
+
+
 end
