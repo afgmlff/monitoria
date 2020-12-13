212d707226f34e64ae501e5bf62cf3a8afd61d82
afgmlff <aeciofgm@gmail.com>
2020-12-13 17:30:31 -0300

    test 12: activity controller get #index successful

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -1,5 +1,12 @@
 # frozen_string_literal: true
 
 class ActivitiesController < ApplicationController
-
+  ##
+  # Ação index da classe Activity.
+  # Renderiza a view index, que exibe todas as monitorias criadas
+  # GET /activities
+  def index
+    # Lista todas as monitorias
+    @activities = Activity.all
+  end
 end
