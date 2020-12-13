668beba705a44b218ac0065e314799b5e2b3938d
afgmlff <aeciofgm@gmail.com>
2020-12-13 17:26:11 -0300

    test 11: edit, update and destroy fail

--- spec/controllers/disciplines_controller_spec.rb
+++ spec/controllers/disciplines_controller_spec.rb
@@ -49,5 +49,46 @@ RSpec.describe DisciplinesController, type: :controller do
       end
     end
   end
+
+
+  describe 'GET #edit' do
+    it 'assigns the requested discipline as @discipline' do
+      discipline = Discipline.create! valid_attributes
+      get :edit, params: { id: discipline.to_param }, session: valid_session
+      expect(assigns(:discipline)).to eq(discipline)
+    end
+  end
+
+  describe 'PUT #update' do
+    context 'with valid params' do
+      let(:new_attributes) do
+        { name: 'test 2.0' }
+      end
+
+      it 'updates the requested discipline' do
+        discipline = Discipline.create! valid_attributes
+        put :update, params: { id: discipline.to_param, discipline: new_attributes }, session: valid_session
+        discipline.reload
+        expect(discipline.name).to eq(new_attributes[:name])
+      end
+    end
+
+    context 'with invalid params' do
+      it 'assigns the discipline as @discipline' do
+        discipline = Discipline.create! valid_attributes
+        put :update, params: { id: discipline.to_param, discipline: invalid_attributes }, session: valid_session
+        expect(assigns(:discipline)).to eq(discipline)
+      end
+    end
+  end
+
+  describe 'DELETE #destroy' do
+    it 'destroys the requested discipline' do
+      discipline = Discipline.create! valid_attributes
+      expect do
+        delete :destroy, params: { id: discipline.to_param }, session: valid_session
+      end.to change(Discipline, :count).by(-1)
+    end
+  end
   
 end
