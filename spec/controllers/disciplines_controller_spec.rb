c58724e7a1084f98ddefad76c177d56a26d15420
afgmlff <aeciofgm@gmail.com>
2020-12-13 17:15:23 -0300

    test 9: discipline controller get #index fail

--- spec/controllers/disciplines_controller_spec.rb
+++ spec/controllers/disciplines_controller_spec.rb
@@ -15,5 +15,11 @@ RSpec.describe DisciplinesController, type: :controller do
 
   let(:disciplines) { create_list :discipline, 1 }
 
+  describe 'GET #index' do
+    it 'returns a success response' do
+      get :index
+      expect(assigns(:disciplines)).to eq disciplines
+    end
+  end
 
 end
