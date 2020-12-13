604e5189d9d113ae9e05f146713d54b9bf71e445
afgmlff <aeciofgm@gmail.com>
2020-12-13 17:02:27 -0300

    test 6: user controller get #show successful

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,5 +10,13 @@ class UsersController < ApplicationController
     @users = User.all
   end
 
+  ##
+  # GET /user/:id
+  # Demonstração em detalhes os dados de um registro anteriormente criado
+  # para visualização
+  def show
+    # De acordo com o id passado pelo usuário (quando clica no usuário) as informações são atualizadas
+    @user = User.find(params[:id])
+  end
 
 end
