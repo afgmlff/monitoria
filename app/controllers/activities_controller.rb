6b8609a44a7363d30bcda7facf13e9fdc787dd75
afgmlff <aeciofgm@gmail.com>
2020-12-13 17:38:11 -0300

    test 15: update and delete successful

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -60,6 +60,48 @@ class ActivitiesController < ApplicationController
     @activity = Activity.find(params[:id])
   end
 
+  ##
+  # Método responsável por atualizar um registro com os dados inseridos em Edit.
+  # Recebe os dados da view Edit e faz o tratamento dos dados modificados pelo usuário.
+  # Caso os dados sejam válidos, o registro é atualizado no banco, caso não sejam, não salva
+  # Redireciona para a página Index
+  # PATCH/PUT /activities/:id
+  def update
+    # Encontra a monitoria com o id passado como parâmetro
+    @activity = Activity.find(params[:id])
+    # Se os dados passados atendem os requisitos estipulados na Model
+    if @activity.update(activitie_params)
+      # É renderizado para Index da monitoria com uma mensagem de sucesso
+      flash[:notice] = 'Monitoria atualizada com sucesso'
+      redirect_to activities_path
+    else
+      # Se não foi possivel fazer update, é renderizado para Index da monitoria com uma mensagem de erro
+      flash[:notice] = 'Não foi possivel editar'
+      redirect_to activities_path
+    end
+  end
+
+  ##
+  # Método responsável por excluir um registro salvo na tabela.
+  # Redireciona para a view index.
+  # DELETE /activities/:id
+  def destroy
+    # Encontra a monitoria com o id passado como parâmetro
+    @activity = Activity.find(params[:id])
+    begin
+      # Se foi possivel apagar o dado, retrona uma mensagem de sucesso
+      @activity.destroy!
+      flash[:notice] = 'Monitoria apagada com sucesso'
+    rescue StandardError => e
+      # Se não foi possivel apagar, aparece uma mensagem de erro
+      flash[:notice] = e
+    ensure
+      # É renderizado para Index das monitoria
+      redirect_to activities_path
+    end
+  end
+
+
   private
 
   ##
