3e65e84a1f18e5c055c34bb656397f0e75b3d32c
afgmlff <aeciofgm@gmail.com>
2020-12-13 16:56:04 -0300

    test 5: user controller get #index successful

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,14 @@
 # frozen_string_literal: true
 
 class UsersController < ApplicationController
+  ##
+  # Ação index da classe User.
+  # Renderiza a view index, que exibe todas as monitorias criadas
+  # GET /users
+  def index
+    # Lista todas as monitorias
+    @users = User.all
+  end
+
 
 end
