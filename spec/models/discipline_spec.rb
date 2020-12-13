93a11e793214c9da572518830c9c77578a4e8f8d
afgmlff <aeciofgm@gmail.com>
2020-12-13 16:46:33 -0300

    test 3: discipline model validates fail

--- spec/models/discipline_spec.rb
+++ spec/models/discipline_spec.rb
@@ -3,5 +3,21 @@
 require 'rails_helper'
 
 RSpec.describe Discipline, type: :model do
+  describe '#discipline?' do
+    context 'when everything is normal' do
+      it 'be_valid' do
+        discipline = build(:discipline)
+        expect(discipline).to be_valid
+      end
+    end
+  end
 
+  describe 'validation' do
+    context 'when creating a discipline with a blank name' do
+      it 'not be_valid' do
+        discipline = build(:discipline, name: '')
+        expect(discipline).not_to be_valid
+      end
+    end
+  end
 end
