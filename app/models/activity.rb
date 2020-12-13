09a659bd853bcb6fd568f48d4f5a3009dd134302
afgmlff <aeciofgm@gmail.com>
2020-12-13 16:50:13 -0300

    test 4: activity model validates successful

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -6,5 +6,19 @@
 # Precisa ter o campo de subject (disciplina)
 # Em métodos com ações de create e update, é necessário a verificação do método check_date
 class Activity < ApplicationRecord
+  belongs_to :user
 
+  validates :subject, presence: true
+
+  validate :check_date, on: %i[create update]
+  ##
+  # Permite atualização com base em decorrência real de um período (data final superior a data inicial)
+  # Retorna "true" caso em caso de data final superior a data inicial ou retorna "false" caso contrário
+  def check_date
+    if start_date.nil? || end_date.nil? || (end_date < start_date)
+      errors.add(:end_date, 'inválida')
+      return false
+    end
+    true
+  end
 end
